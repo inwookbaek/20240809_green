e4743b5178e0e289b35500d6b9c36d26b3041397
inwookbaek <iwbaek@gmil.com>
2025-02-15 07:50:03 +0900

    20250215

--- 06.spring/sp11_jwt_copy/src/main/java/com/lec/board/repository/APIUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lec.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lec.board.domain.APIUser;
-
-public interface APIUserRepository extends JpaRepository<APIUser, String>{
-
-}
--- 06.spring/sp11_jwt_copy/src/main/java/com/lec/board/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lec.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lec.board.domain.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
