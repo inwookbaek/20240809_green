eb9c56b5803bc934d8d6b0539726dfdcd6d25081
inwookbaek <iwbaek@gmil.com>
2025-02-16 20:57:38 +0900

    20250216

---
+++ 08.typescript/01.basic.tsx
@@ -0,0 +1,12 @@
+type Person = {
+  name: string;
+  age: number;
+};
+
+const person: Person = {
+  name: "Alice",
+  age: 30
+};
+
+console.log(person.name); // Alice
+console.log(person.age); // 30
